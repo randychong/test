d17c04f4f413d85d3c1324a9ca0af72b72552aa8
Randy Chong <randychong27@yahoo.com>
2021-08-09 09:09:20 -0500

    removed unused function

--- pokemon.py
+++ pokemon.py
@@ -1,3 +1,5 @@
+import time
+
 class Pokemon:
     def __init__(self, name, health, attack, defense, special, special_attack, energy_req, energy = 0) -> None:
         self.name = name
@@ -16,105 +18,131 @@ class Pokemon:
         if self.health <= 400:
             self.health += 100
         else:
-            print(f"{self.name} has too much health to use a potion.")
-
-    def view_stats(self):
-        print(f"Health: {self.health}\nAttack: {self.attack}\nDefense: {self.defense}")
+            time.sleep(1)
+            print(f"\n{self.name} has too much health to use a potion.")
 
     def gain_energy(self, energy):
         self.energy += energy
 
     def use_special_attack(self):
-            print(f"{self.name} uses {self.special} dealing {self.special_attack} damage!!!\n")
-            self.energy -= self.energy_req
+        time.sleep(1)
+        print(f"{self.name} uses {self.special} dealing {self.special_attack} damage!!!\n")
+        self.energy -= self.energy_req
 
     def check_pokemon_hp(self):
         if self.health <= 0:
+            time.sleep(1)
             print(f"{self.name} has taken too much damage to continue fighting...\n")
+            time.sleep(1)
             print(f"{self.name} lost the battle.")
+            time.sleep(1)
             exit()
         else:
             pass
 
-lucario = Pokemon("Lucario", 500, 80, 40, "Aura Sphere", 9999, 400)
-mewtwo = Pokemon("MewTwo", 1000, 100, 50, "Psystrike", 400, 800)
+main_pokemon = Pokemon("Lucario", 500, 80, 40, "Aura Sphere", 9999, 400)
+enemy_pokemon = Pokemon("Mewtwo", 1000, 100, 50, "Psystrike", 400, 800)
 
 def main_menu():
-    if lucario.energy >= lucario.energy_req:
-        lucario.use_special_attack()
-        mewtwo.take_damage(lucario.special_attack)
+    time.sleep(1)
+    if main_pokemon.energy >= main_pokemon.energy_req:
+        main_pokemon.use_special_attack()
+        enemy_pokemon.take_damage(main_pokemon.special_attack)
     else:
         pass
 
-    if mewtwo.energy >= mewtwo.energy_req:
-        mewtwo.use_special_attack()
-        lucario.take_damage(mewtwo.special_attack)
+    if enemy_pokemon.energy >= enemy_pokemon.energy_req:
+        enemy_pokemon.use_special_attack()
+        main_pokemon.take_damage(enemy_pokemon.special_attack)
     else:
         pass
 
-    lucario.check_pokemon_hp()
-    mewtwo.check_pokemon_hp()
+    main_pokemon.check_pokemon_hp()
+    enemy_pokemon.check_pokemon_hp()
 
     choice = int(input(f"""
-    LUCARIO VS MEWTWO
+    {main_pokemon.name} VS {enemy_pokemon.name}
 
-    A wild {mewtwo.name} has appeared! What do you want {lucario.name} to do?
-    1. Attack {mewtwo.name}
-    2. Guard against {mewtwo.name}'s next attack
+    A wild {enemy_pokemon.name} has appeared! What do you want {main_pokemon.name} to do?
+    1. Attack {enemy_pokemon.name}
+    2. Guard against {enemy_pokemon.name}'s next attack
     3. Use a Potion
-    4. Check on {lucario.name} and {mewtwo.name}'s stats
+    4. Check on {main_pokemon.name} and {enemy_pokemon.name}'s stats
     5. Run Away
 
     """))
     while choice != 5:
         if choice == 1:
-            print(f"\n{lucario.name} jabs Mewtwo for {lucario.attack - mewtwo.defense} damage!\n")
-            damage_dealt = int(f"{lucario.attack - mewtwo.defense}")
-            mewtwo.take_damage(damage_dealt)
-            mewtwo.gain_energy(damage_dealt)
-            print(f"{mewtwo.name} counters back dealing {mewtwo.attack - lucario.defense} damage!\n")
-            damage_taken = int(f"{mewtwo.attack - lucario.defense}")
-            lucario.take_damage(damage_taken)
-            lucario.gain_energy(damage_taken)
+            time.sleep(1)
+            print(f"\n{main_pokemon.name} jabs {enemy_pokemon.name} for {main_pokemon.attack - enemy_pokemon.defense} damage!\n")
+            damage_dealt = int(f"{main_pokemon.attack - enemy_pokemon.defense}")
+            enemy_pokemon.take_damage(damage_dealt)
+            enemy_pokemon.gain_energy(damage_dealt)
+            time.sleep(1)
+            print(f"{enemy_pokemon.name} counters back dealing {enemy_pokemon.attack - main_pokemon.defense} damage!\n")
+            damage_taken = int(f"{enemy_pokemon.attack - main_pokemon.defense}")
+            main_pokemon.take_damage(damage_taken)
+            main_pokemon.gain_energy(damage_taken)
+            time.sleep(1)
             main_menu()
         elif choice == 2:
-            print(f"\n{lucario.name} shields {mewtwo.name}'s attack and only takes 10 damage!\n")
-            lucario.gain_energy(100)
+            time.sleep(1)
+            print(f"\n{main_pokemon.name} shields {enemy_pokemon.name}'s attack and only takes 10 damage!\n")
+            time.sleep(1)
+            print(f"You feel as if {main_pokemon.name} is gaining more energy.\n")
+            main_pokemon.take_damage(10)
+            main_pokemon.gain_energy(100)
+            time.sleep(1)
             main_menu()
         elif choice == 3:
-            lucario.heal_damage()
-            print(f"\nYou noticed that {mewtwo.name} gained some extra energy.\n")
-            mewtwo.gain_energy(200)
+            time.sleep(1)
+            main_pokemon.heal_damage()
+            time.sleep(1)
+            print(f"\nYou noticed that {enemy_pokemon.name} gained some extra energy.\n")
+            enemy_pokemon.gain_energy(200)
+            time.sleep(1)
             main_menu()
         elif choice == 4:
+            time.sleep(1)
             print(f"""
             -------------------------------------------------------------------
             -------------------------------------------------------------------
-            {lucario.name}
-            Health: {lucario.health}
-            Attack: {lucario.attack}
-            Defense: {lucario.defense}
-            Special Move: {lucario.special} || Damage: {lucario.special_attack} || Energy Req: {lucario.energy_req}
-            Energy: {lucario.energy}
-            -------------------------------------------------------------------
-            -------------------------------------------------------------------
-            {mewtwo.name}
-            Health: {mewtwo.health}
-            Attack: {mewtwo.attack}
-            Defense: {mewtwo.defense}
-            Special Move: {mewtwo.special} || Damage: {mewtwo.special_attack} || Energy Req: {mewtwo.energy_req}
-            Energy: {mewtwo.energy}
+            {main_pokemon.name}
+            Health: {main_pokemon.health}
+            Attack: {main_pokemon.attack}
+            Defense: {main_pokemon.defense}
+            Special Move: {main_pokemon.special} || Damage: {main_pokemon.special_attack} || Energy Req: {main_pokemon.energy_req}
+            Energy: {main_pokemon.energy}
             -------------------------------------------------------------------
             -------------------------------------------------------------------
             """)
+
+            time.sleep(2)
+            print(f"""
+            -------------------------------------------------------------------
+            -------------------------------------------------------------------
+            {enemy_pokemon.name}
+            Health: {enemy_pokemon.health}
+            Attack: {enemy_pokemon.attack}
+            Defense: {enemy_pokemon.defense}
+            Special Move: {enemy_pokemon.special} || Damage: {enemy_pokemon.special_attack} || Energy Req: {enemy_pokemon.energy_req}
+            Energy: {enemy_pokemon.energy}
+            -------------------------------------------------------------------
+            -------------------------------------------------------------------
+            """)
+            time.sleep(2)
             main_menu()
         elif choice == 5:
-            print("""
-            You and Lucario shamefully run away from battle...\n
+            time.sleep(1)
+            print(f"""
+            You and {main_pokemon.name} shamefully run away from battle...\n
             """)
+            time.sleep(1)
             exit()
         else:
-            print("Please choose a valid menu option.")
+            time.sleep(1)
+            print("Please choose a valid menu option.\n")
+            time.sleep(1)
             main_menu()
 
 main_menu()

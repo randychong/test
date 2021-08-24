61635a43b83fa50cf7486c58733a48231683856f
Randy Chong <randychong27@yahoo.com>
2021-08-24 09:08:15 -0500

    completed functionality for todo list

---
+++ todo.js
@@ -0,0 +1,37 @@
+const root = document.querySelector(".root");
+const mainContainer = document.createElement("div");
+const input = document.createElement("input");
+const button = document.createElement("button");
+const taskContainer = document.createElement("div");
+const list = document.createElement("ul");
+
+mainContainer.className = "mainContainer";
+input.className = "input";
+button.className = "button";
+button.innerHTML = "Add Task";
+taskContainer.className = "taskContainer";
+list.className = "taskList";
+
+mainContainer.append(input, button);
+taskContainer.append(list);
+root.append(mainContainer, taskContainer);
+
+function addTask() {
+  const input = document.querySelector(".input").value;
+  const list = document.querySelector(".taskList");
+  const task = document.createElement("li");
+
+  task.append(input);
+  list.append(task);
+}
+
+button.addEventListener("click", function () {
+  addTask();
+});
+
+input.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    button.click();
+  }
+});
